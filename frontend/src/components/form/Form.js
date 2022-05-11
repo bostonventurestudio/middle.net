64f4591704b372e98ca3d0fe1c592d0bd6897e9c
AbdulWajid001 <abdul.wajid@arbisoft.com>
2022-05-11 19:04:09 +0500

    feat: handled form submit

--- frontend/src/components/form/Form.js
+++ frontend/src/components/form/Form.js
@@ -1,17 +1,27 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import Map from "../locationSearch/LocationSearch";
+import {APIBaseURL} from "../../config";
 
 class Form extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
+            name: '',
             address: '',
             position: {
                 lat: 0,
                 lng: 0
             },
         };
+        this.setName = this.setName.bind(this);
         this.setCurrentPosition = this.setCurrentPosition.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    setName(event) {
+        this.setState({name: event.target.value});
     }
 
     setCurrentPosition(position) {
@@ -22,15 +32,31 @@ class Form extends Component {
         navigator.geolocation.getCurrentPosition(this.setCurrentPosition);
     }
 
+    handleSubmit = event => {
+        event.preventDefault();
+        const data = JSON.stringify({
+            name: this.state.name,
+            latitude: this.state.position.lat,
+            longitude: this.state.position.lng,
+            address: this.state.address
+        })
+        axios.post(APIBaseURL, {data})
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    };
+
+
     render() {
         return (
             <div>
-                <form className="form">
+                <form className="form" onSubmit={this.handleSubmit}>
                     <div className="input-holder">
-                        <input type="text" placeholder="Enter your nickname or initials"/>
+                        <input type="text" placeholder="Enter your nickname or initials" value={this.state.name} onChange={this.setName} required/>
                     </div>
                     <Map state={this.state} zoom={16}/>
-                    <button type="button" className="btn-primary">Middle <i className="icon-right-2"/></button>
+                    <button type="submit" className="btn-primary">Middle <i className="icon-right-2"/></button>
                 </form>
             </div>
         );
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
 export const GoogleMapsAPI = "AIzaSyBGyR3-Ga1LOz5YaqCQZDs4BOz0YO34GoU";
+export const APIBaseURL = "http://127.0.0.1:8000/api/location/";
