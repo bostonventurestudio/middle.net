98f6f9483a3f1656a65d19e2bb5acf5224229eaf
AbdulWajid001 <abdul.wajid@arbisoft.com>
2022-05-13 00:16:41 +0500

    feat: added nearbyPlacesAPI

--- backend/apps/core/urls.py
+++ backend/apps/core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, re_path
 
-from backend.apps.core.views import LocationView
+from backend.apps.core.views import LocationView, NearbyPlacesView
 
 urlpatterns = [
     path('location/', LocationView.as_view()),
     re_path('location/(.*)', LocationView.as_view(), name='get-locations'),
-
+    re_path('nearby-places/', NearbyPlacesView.as_view(), name='get-locations'),
 
 ]
--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -1,5 +1,7 @@
 import json
 
+import requests
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -12,7 +14,19 @@ class LocationView(APIView):
         return Response(LocationModelSerializer(Location.objects.filter(slug=slug), many=True).data)
 
     def post(self, request):
-        serializer = LocationSerializer(data=json.loads(request.data.get('data')))
+        serializer = LocationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         location = serializer.save()
         return Response(LocationModelSerializer(Location.objects.filter(slug=location.slug), many=True).data)
+
+
+class NearbyPlacesView(APIView):
+    def post(self, request):
+        try:
+            response = requests.request("GET", request.data.get('url'))
+        except Exception as e:
+            return Response(e.args, status=status.HTTP_400_BAD_REQUEST)
+        if response.status_code == status.HTTP_200_OK:
+            return Response(json.loads(response.text)['results'][:5])
+        else:
+            return Response(response.text, status=response.status_code)
