2047f50a2a0165c221e9e737500d4a737bb4e016
AbdulWajid001 <abdul.wajid@arbisoft.com>
2022-05-13 11:44:33 +0500

    feat: added images for places

--- frontend/src/components/nearbyPlace/NearbyPlace.js
+++ frontend/src/components/nearbyPlace/NearbyPlace.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import image from "../../images/img-1.jpg";
 import {Rating} from "react-simple-star-rating";
-import {GoogleMapDirectionLink} from "../../config";
+import {GoogleMapDirectionLink, GooglePlaceImageURL} from "../../config";
 
 class NearbyPlace extends Component {
     render() {
@@ -11,7 +11,8 @@ class NearbyPlace extends Component {
                     <span className="num">{this.props.index}</span>
                     <div className="img-detail-block">
                         <div className="img-holder">
-                            <img src={image} alt=""/>
+                            {this.props.place.photos.length > 0 ? <img src={`${GooglePlaceImageURL}?photoreference=${this.props.place.photos[0].photo_reference}&sensor=false&maxheight=70&maxwidth=60&key=AIzaSyAE6enNZ1pwRa0RVk6dZqqXh9HTkijOgX0`} alt=""/> :
+                            <img src={image} alt=""/>}
                         </div>
                         <div className="detail-block">
                             <span className="title">{this.props.place.name}</span>
@@ -23,19 +24,14 @@ class NearbyPlace extends Component {
                     <div className="rating-time-block">
                         <ul className="rating-list">
                             <li>{this.props.place.rating ? this.props.place.rating : 0}</li>
-                            {/*<li><i className="icon-star"/></li>*/}
-                            {/*<li><i className="icon-star"/></li>*/}
-                            {/*<li><i className="icon-star"/></li>*/}
-                            {/*<li><i className="icon-star"/></li>*/}
-                            {/*<li><i className="icon-star"/></li>*/}
-                            <Rating ratingValue={this.props.place.rating ? this.props.place.rating*20 : 0} readonly={true} size={15} />
+                            <Rating ratingValue={this.props.place.rating ? this.props.place.rating * 20 : 0} readonly={true} size={15}/>
                         </ul>
                         <div className="timing-info">
                             <span className="open">{this.props.place.opening_hours && this.props.place.opening_hours.open_now ? "Open" : "Close"}</span>
                         </div>
                     </div>
                     <div className="directions-block">
-                        <a href={GoogleMapDirectionLink+`?api=1&destination=${this.props.place.geometry.location.lat},${this.props.place.geometry.location.lng}`}>
+                        <a href={GoogleMapDirectionLink + `?api=1&destination=${this.props.place.geometry.location.lat},${this.props.place.geometry.location.lng}`}>
                             <div className="icon-holder">
                                 <i className="icon-directions"/>
                             </div>
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,3 +3,4 @@ export const LocationAPIURL = "http://127.0.0.1:8000/api/location/";
 export const NearbyPlacesAPIURL = "http://127.0.0.1:8000/api/nearby-places/";
 export const GooglePlacesAPIBaseURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
 export const GoogleMapDirectionLink = "https://www.google.com/maps/dir/";
+export const GooglePlaceImageURL = "https://maps.googleapis.com/maps/api/place/photo";
