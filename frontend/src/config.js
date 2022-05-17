376e73eca761eb9de7e5fc8ce58209c273bbc1b6
AbdulWajid001 <abdul.wajid@arbisoft.com>
2022-05-17 12:01:41 +0500

    refactor: updated config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
-export const LocationAPIURL = "http://127.0.0.1:8000/api/location/";
-export const NearbyPlacesAPIURL = "http://127.0.0.1:8000/api/nearby-places/";
+export const APIBaseURL = "http://127.0.0.1:8000";
+export const LocationAPIURL = `${APIBaseURL}/api/location/`;
+export const NearbyPlacesAPIURL = `${APIBaseURL}/api/nearby-places/`;
 export const GooglePlacesAPIBaseURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
 export const GoogleMapDirectionLink = "https://www.google.com/maps/dir/";
 export const GooglePlaceImageURL = "https://maps.googleapis.com/maps/api/place/photo";
