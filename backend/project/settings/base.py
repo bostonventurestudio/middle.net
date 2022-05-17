3c0a8107f33eac21fd9ebafd6df4f79706674b24
AbdulWajid001 <abdul.wajid@arbisoft.com>
2022-05-17 12:41:50 +0500

    refactor: updated base.py

--- backend/project/settings/base.py
+++ backend/project/settings/base.py
@@ -166,7 +166,7 @@ if DEBUG:
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+# STATIC_ROOT = 'static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
