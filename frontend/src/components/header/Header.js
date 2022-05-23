20d4cfec270cd820b246e0495628b10137d07608
AbdulWajid001 <abdul.wajid@arbisoft.com>
2022-05-23 12:37:21 +0500

    refactor: made header changes

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,14 +1,15 @@
 import logo from "../../images/logo.svg";
 import React from 'react';
+import {Link} from "react-router-dom";
 
-export const Header = (props) => {
+export const Header = () => {
     return (
         <header id="header">
             <div className="container">
                 <div className="logo">
-                    <a href="/"><img src={logo} alt="Middle"/></a>
+                    <Link to="/"><img src={logo} alt="Middle"/></Link>
                 </div>
-                <span className="sub-text">Meet in the Middle is the easiest way to find where to meet friends.</span>
+                <span className="sub-text">meet in the middle</span>
             </div>
         </header>
     )
