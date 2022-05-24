d025c22cb580c0675d872548d64da24fee6eb0ed
AbdulWajid001 <abdul.wajid@arbisoft.com>
2022-05-24 11:42:15 +0500

    fix: home url

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
         <footer id="footer">
             <div className="container">
                 <div className="links">
-                    <Link className="link" to="/">Home</Link>
+                    <a className="link" href="/">Home</a>
                     <a className="link" href="https://www.info.middle.net/about">About</a>
                     <a className="link-last" href="https://www.info.middle.net/contact">Contact</a>
                 </div>
