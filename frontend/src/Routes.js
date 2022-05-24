13f772c87cc90d1e73583f3cf5d2d3a556923504
AbdulWajid001 <abdul.wajid@arbisoft.com>
2022-05-24 05:51:29 +0500

    refactor: removed unused code

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,7 +1,6 @@
 import React from "react";
-import {BrowserRouter as Router, Navigate, Route, Routes as Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes as Switch} from "react-router-dom";
 import {Footer, Header, Home, NotFound} from './components';
-import TopLocations from "./components/topLocations/TopLocations";
 
 export default function Routes() {
     return (
