4980f05c39fde2c5b4ae5de4d2bbaf6cc9561d66
AbdulWajid001 <abdul.wajid@arbisoft.com>
2022-07-15 12:40:16 +0500

    feat: add type field to location model

---
+++ backend/apps/core/constants.py
@@ -0,0 +1,10 @@
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  Copyright (c) 2022, Boston Venture Studio, Inc - https://www.bvs.net/
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+LOCATION = 'location'
+CENTER = 'center'
+LOCATION_TYPES = (
+    (LOCATION, 'Location'),
+    (CENTER, 'Center')
+)
--- backend/apps/core/models.py
+++ backend/apps/core/models.py
@@ -8,12 +8,15 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
+from apps.core.constants import LOCATION, LOCATION_TYPES
+
 
 class Location(models.Model):
     latitude = models.DecimalField(max_digits=8, decimal_places=5, db_index=True)
     longitude = models.DecimalField(max_digits=8, decimal_places=5, db_index=True)
     google_place_id = models.CharField(max_length=200, null=True, blank=True)
     address = models.CharField(max_length=200, null=True, blank=True)
+    type = models.CharField(choices=LOCATION_TYPES, max_length=20, default=LOCATION)
     created_at = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=8, blank=True, null=True)
 
